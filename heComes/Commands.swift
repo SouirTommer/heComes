f1a92b2eba16d48414ea0aa04d71dd53859a7f51
Tommer <souirtommer@gmail.com>
2023-03-14 17:42:42 +0800

    fix bug

---
+++ heComes/Commands.swift
@@ -0,0 +1,13 @@
+//
+//  commands.swift
+//  
+//
+//  Created by teddy juhlin-henricson on 2022-05-25.
+//
+
+import Foundation
+
+enum Commands: String {
+    case hello = "!hello"
+    case back = "!back"
+}
--- heComes/ContentView.swift
+++ heComes/ContentView.swift
@@ -6,14 +6,64 @@
 //
 
 import SwiftUI
+import Sword
 
 struct ContentView: View {
+    
+    @State var isTrue = true
+    //discord bot token
+    let bot = Sword(token: "add token here!")
+    let date = Date()
+    let df = DateFormatter()
+    
+    init(){
+        df.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        bot.editStatus(to: "playing", playing: "chrome")
+        
+        bot.on(.messageCreate) { data in
+            //Requires a backend server
+        var msg = data as! Message
+        switch Commands(rawValue: msg.content) {
+        case .hello?:
+            msg.reply(with: "hello!")
+        case .back?:
+            msg.reply(with: "GoBack GoBack!")
+        default:
+            break
+        }
+            
+    }
+
+    bot.on(.ready) { data in
+        let user = data as! User
+        print("\n\t\(user.username ?? "Bot") says 'Hello there!'\n")
+    }
+
+    bot.connect()
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button(action: {
+                isTrue.toggle()
+                if(isTrue){
+                    // modify to your channel id
+                    bot.send("老母不在 " + df.string(from: date), to: 1085133432193036358)
+                } else{
+                    
+                    bot.send("老母黎咗 " + df.string(from: date), to: 1085133432193036358)
+                }
+                
+                    }) {
+                        Circle()
+                            .fill(isTrue ? Color.green : Color.red)
+                            .frame(width: 200, height: 200)
+                            .overlay(Text(isTrue ? "老母不在 Good" : "老母黎咗 Bad")
+                                        .foregroundColor(.white)
+                                        .font(.headline))
+                            .shadow(color: .gray, radius: 5, x:0, y:2)
+                    }
         }
         .padding()
     }
