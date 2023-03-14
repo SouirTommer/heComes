284d100741c73ad98b592700a8b6ad1c433c5640
Tommer <souirtommer@gmail.com>
2023-03-14 15:26:17 +0800

    Initial Commit

---
+++ heComes/heComesApp.swift
@@ -0,0 +1,17 @@
+//
+//  heComesApp.swift
+//  heComes
+//
+//  Created by ST SE on 14/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct heComesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
