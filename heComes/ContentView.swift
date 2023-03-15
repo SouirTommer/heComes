51800efb7ba671ec4127e59b6ccf59f645af75ad
Tommer <souirtommer@gmail.com>
2023-03-16 00:45:12 +0800

    Modify code and the layout more beautiful and meme(able)!

--- heComes/ContentView.swift
+++ heComes/ContentView.swift
@@ -10,7 +10,6 @@ import Sword
 
 struct ContentView: View {
     
-    @State var isTrue = true
     //discord bot token
     let bot = Sword(token: "add token here!")
     let date = Date()
@@ -44,28 +43,83 @@ struct ContentView: View {
     }
 
     var body: some View {
-        VStack {
-            Button(action: {
-                isTrue.toggle()
-                if(isTrue){
-                    // modify to your channel id
-                    bot.send("老母不在 " + df.string(from: date), to: 1085133432193036358)
-                } else{
-                    
-                    bot.send("老母黎咗 " + df.string(from: date), to: 1085133432193036358)
-                }
+        ZStack{
+            
+            Color(red: 181/255, green: 208/255, blue: 237/255)
+                .ignoresSafeArea(.all)
+            HStack(spacing: 30){
                 
-                    }) {
-                        Circle()
-                            .fill(isTrue ? Color.green : Color.red)
-                            .frame(width: 200, height: 200)
-                            .overlay(Text(isTrue ? "老母不在 Good" : "老母黎咗 Bad")
+                
+                VStack() {
+                    
+                    
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(width: 170, height: 250)
+                        .shadow(color: .gray, radius: 5, x: 0, y: 2)
+                        .overlay(
+                            
+                            Button(action: {
+                                bot.send("老母不在 " + df.string(from: date), to: 1085133432193036358)
+                                
+                            }) {
+                                Circle()
+                                    .fill(Color.red)
+                                    .frame(width: 150, height: 150)
+                                    .overlay(Text("老母 Good")
                                         .foregroundColor(.white)
                                         .font(.headline))
-                            .shadow(color: .gray, radius: 5, x:0, y:2)
-                    }
+                                
+                                
+                            }
+                                .shadow(color: .gray, radius: 5, x: 0, y: 4)
+                            
+                            
+                        )
+                        .offset(y:-100)
+                    
+                    
+                    
+                }
+                
+                
+                
+                VStack(){
+                    
+                    
+                    
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(width: 170, height: 250)
+                        .shadow(color: .gray, radius: 5, x: 0, y: 2)
+                        .overlay(
+                            
+                            Button(action: {
+                                bot.send("老母黎咗 " + df.string(from: date), to: 1085133432193036358)
+                            }) {
+                                Circle()
+                                    .fill(Color.red)
+                                    .frame(width: 150, height: 150)
+                                    .overlay(Text("老母黎咗 Bad")
+                                        .foregroundColor(.white)
+                                        .font(.headline))
+                            }
+                                .shadow(color: .gray, radius: 5, x: 0, y: 4)
+                        )
+                        .offset(y:-100)
+                    
+                }
+            }
+            VStack{
+                Spacer()
+                Image("image1")
+                    .resizable()
+                    .shadow(color: .black, radius: 5, x: 0, y: 4)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 100)
+                
+            }
         }
-        .padding()
     }
 }
 
